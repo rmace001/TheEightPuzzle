51d4c31c5c4289a1367c41b96367581e408d6c22
Rogelio Macedo <rmace001@ucr.edu>
2018-10-31 05:32:11 +0000

    Add Print() Function

--- TheEightPuzzle.cpp
+++ TheEightPuzzle.cpp
@@ -9,31 +9,49 @@ using namespace std;
 
 struct node {
   unsigned short arr[3][3];
+  
+  void print(){
+    for (short i=0; i<3; i++){
+      for (short j=0; j<3; j++){
+        if (j<2){
+          cout << arr[i][j] << " ";
+        }
+        else{
+          cout << arr[i][j] << endl;
+        }
+      }
+    }
+  }
 };
 
 
 
 
 int main (){
-  // int isDefault = -1;
-  // cout << "Welcome to Rogelio Macedo's 8-puzzle solver. \n";
-  // cout << "Enter \"1\" to use a default puzzle, or \"2\" to enter your own puzzle \n";
-  // cout << "(Enter -1 to quit)\n\n";
-  // cin >> isDefault;
-  // if (isDefault == 1){
-  //   cout << "You selected 1.\n";
-  // }
-  // else if (isDefault == 2){
-  //   cout << "You selected 2.\n";
-  // }
+  int isDefault = -1;
+  cout << "Welcome to Rogelio Macedo's 8-puzzle solver. \n";
+  cout << "Enter \"1\" to use a default puzzle, or \"2\" to enter your own puzzle \n";
+  cout << "(Enter -1 to quit)\n\n";
+  cin >> isDefault;
+  if (isDefault == 1){
+    cout << "You selected 1.\n";
+  }
+  else if (isDefault == 2){
+    cout << "You selected 2.\n";
+  }
   
+  else if (isDefault == -1){
+    cout << "Bye!\n";
+    return 0;
+  }
   
-  // else if (isDefault == -1){
-  //   cout << "Bye!\n";
-  //   return 0;
-  // }
   
   /////////////NODE TESTING////////////
+  cout << "printing default node n" << endl;
+  node n; 
+  n.print();
+  
+  
   // Node n;
   // n.x = 1;
   // cout << n.x << endl;
