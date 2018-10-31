a6f0e28c5ba05c7e1a495178902b255aab834122
Rogelio Macedo <rmace001@ucr.edu>
2018-10-31 06:15:01 +0000

    Add BuildArray() Function

--- TheEightPuzzle.cpp
+++ TheEightPuzzle.cpp
@@ -22,35 +22,128 @@ struct node {
       }
     }
   }
+  
+  void buildArray(unsigned short a, unsigned short b, unsigned short c,
+                  unsigned short d, unsigned short e, unsigned short f,
+                  unsigned short g, unsigned short h, unsigned short i)
+  {
+    arr[0][0] = a;
+    arr[0][1] = b;
+    arr[0][2] = c;
+    arr[1][0] = d;
+    arr[1][1] = e;
+    arr[1][2] = f;
+    arr[2][0] = g;
+    arr[2][1] = h;
+    arr[2][2] = i;
+  }
 };
 
 
 
 
 int main (){
+  
   int isDefault = -1;
+    
   cout << "Welcome to Rogelio Macedo's 8-puzzle solver. \n";
   cout << "Enter \"1\" to use a default puzzle, or \"2\" to enter your own puzzle \n";
   cout << "(Enter -1 to quit)\n\n";
   cin >> isDefault;
-  if (isDefault == 1){
-    cout << "You selected 1.\n";
+  while (isDefault != -1){
+    if (isDefault == 1){
+      cout << "You selected 1.\n";
+    }
+    else if (isDefault == 2){
+      cout << "You selected 2.\n";
+      unsigned short a, b, c, d, e, f, g, h, i;
+      cout << "Enter each element of the puzzle separately," << endl;
+      cout << "and the number 0 as the blank space." << endl;
+      cout << "\"Error\" prints if value entered is not a number" << endl;
+      cin >> a;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> a;
+      }
+      cin >> b;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> b;
+      }
+      cin >> c;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> c;
+      }
+      cin >> d;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> d;
+      }
+      cin >> e;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> e;
+      }
+      cin >> f;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> f;
+      }
+      cin >> g;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> g;
+      }
+      cin >> h;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> h;
+      }
+      cin >> i;
+      while(cin.fail()) {
+        cout << "Error" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> i;
+      }
+      node n;
+      n.buildArray(a, b, c,
+                    d, e, f, 
+                    g, h, i);
+      n.print();
+    }
+    
+    else if (isDefault == -1){
+      cout << "Bye!\n";
+      return 0;
+    }
+   
+    
+    /////////////NODE TESTING////////////
+    cout << "printing default node n" << endl;
+    node n; 
+    n.print();
+    cout << "Enter \"1\" to use a default puzzle, or \"2\" to enter your own puzzle \n";
+    cout << "(Enter -1 to quit)\n\n";
+    cin >> isDefault;
   }
-  else if (isDefault == 2){
-    cout << "You selected 2.\n";
-  }
-  
-  else if (isDefault == -1){
-    cout << "Bye!\n";
-    return 0;
-  }
-  
-  
-  /////////////NODE TESTING////////////
-  cout << "printing default node n" << endl;
-  node n; 
-  n.print();
-  
   
   // Node n;
   // n.x = 1;
